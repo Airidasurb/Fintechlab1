8f85c42abbc4bdf6b87fec98fc23369a54fe1b1a
Airidasurb <airidas.ur1@gmail.com>
2025-03-09 12:57:37 +0200

    text color change functionality added

--- app/src/main/java/com/example/fintechlab1/MainActivity.java
+++ app/src/main/java/com/example/fintechlab1/MainActivity.java
@@ -1,31 +1,35 @@
 package com.example.fintechlab1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        tvMain = findViewById(R.id.tvMain);
+
+        Button changeColorButton = findViewById(R.id.btnChangeColor);
+        changeColorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Change the text color of tvMain
+                tvMain.setTextColor(Color.RED);
+            }
         });
     }
 
     public void onBtnChangeTextClick(View view) {
-        TextView tvMain = findViewById(R.id.tvMain);
         tvMain.setText("Sveikas pasaulėli!");
     }
 }
